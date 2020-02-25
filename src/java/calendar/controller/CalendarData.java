61a9f006d3a9c480a50eed6fcebe392135c6600a
whdobbs <whdobbs@192.168.200.44>
2020-02-24 21:07:28 -0600

    Changed SMS and email messages from cancelled to re-scheduled.

--- src/java/calendar/controller/CalendarData.java
+++ src/java/calendar/controller/CalendarData.java
@@ -521,7 +521,7 @@ public class CalendarData
 
             if (fc.isEventChange())
             {
-                smsEventChangeMsg = " has been re-scheduled to ";
+                smsEventChangeMsg = " has been RE-SCHEDULE to ";
                 m.setSubject("RE-SCHEDULE");
             }
             else if ("delete".equals(fc.getAction()))
